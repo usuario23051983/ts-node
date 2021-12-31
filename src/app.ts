a7284532e4861c33c424eac4154cead4f3cb0f83
jmr <javier.marin@hotusa.com>
2021-12-31 13:29:37 +0100

    commit

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port: number = Number(process.env.PORT) || 3000; // set our port
+app.get('/', (req, res) => {
+  res.send('OK!');
+});
+app.listen(port, () => {
+  console.log(`App listening on... ${port}`);
+});
